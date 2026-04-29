e59d8f5573d41787fc26a08d5f5522b84b06a281
looper <774659391@qq.com>
2026-04-30 02:01:14 +0800

    fix: override body min-width in popup to prevent oversized popup window

--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { PopupApp } from "./PopupApp";
 import "../styles.css";
 
+document.body.style.minWidth = "0px";
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <PopupApp />
